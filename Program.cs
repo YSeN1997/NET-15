e0d907da3d70f5c6d6b4903faa055f043faa5829
Usenka_Uahenij <103533323+YSeN1997@users.noreply.github.com>
2022-04-26 22:40:34 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,45 +1,114 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 
-namespace УчетПродуктовТорговойКомпании
+Console.WriteLine("Домашнее задание. Создать список продуктов.");
+
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("Введите количество продуктов:");
+var prodNumber = int.Parse(Console.ReadLine());
+
+
+List<Product> productList = new List<Product>();
+
+
+for (int i = 1; i <= prodNumber; i++)
 {
-    internal class Program
+    
+    Console.WriteLine($"Введите название продукта {i}: ");
+    string productName = Console.ReadLine();
+    
+    Console.WriteLine($"Введите описание продукта {i}: ");
+    string productDescription = Console.ReadLine();
+    
+    Console.WriteLine($"Укажите цену товара в BYN: ");
+    double productPrice =double.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Выберете к какой категории относиться данный продукт: \n Питание - 1 \n Хозтовар - 2 \n Спорттовары - 3  {i}: ") ;
+    string productCategory = Console.ReadLine();
+
+    Product myProduct;
+
+    switch (productCategory)
     {
-        
-        static void Main(string[] args)
-        {
-            Console.Write("Введите количество товаров: ");
-            var objectCount = int.Parse(Console.ReadLine());
-            var list = new List<Product>();
-            for(int i = 0; i < objectCount; i++)
-            {
-                Console.WriteLine("Введите занчение для товора " + (i+1));
-                list.Add(new Product(Console.ReadLine(), int.Parse(Console.ReadLine())));
-            }
-            Console.WriteLine("Товары: ");
-            
-            foreach(var item in list)
-            {
-                Console.WriteLine(String.Format("Имя: {0}, Цена: {1}", item.name, item.price));
-                
-            }
-        }
-        public class Product
-        {
-            public string name;
-            public int price;
-
-            public Product(string name, int price)
-            {
-                this.price = price;
-                this.name = name;
-            }
-        }
-        
-
+        case "1":
+            myProduct = new FoodProduct();
+            break;
+        case "2":
+            myProduct = new HouseholdProduct();
+            break;
+        case "3":
+            myProduct = new SportProduct();
+            break;
+        default: 
+            myProduct = new Product();
+            break;
     }
-        
 
-}
+    var product = new Product();
+
+    product.Name = productName;
+    product.Description = productDescription;
+    product.Price = productPrice;
    
 
+    productList.Add(product);
+
+}
+
+foreach (var product in productList)
+    Console.WriteLine($"Название продукта: {product.Name}, Описание: {product.Description}, цена: {product.Price} "  );
+
+foreach (var myProduct in productList)
+    Console.WriteLine($"Продукт из категории " );
+
+
+
+public class Product
+{
+    public string Name;
+    public string Description;
+    public double Price;
+    
+
+   
+  
+
+    
+
+
+
+
+}
+
+public class FoodProduct : Product
+{
+
+
+}
+
+
+public class HouseholdProduct : Product
+{
+
+
+}
+
+
+
+
+
+public class SportProduct : Product
+{
+   public string sport;
+ 
+
+
+
+}
+
+
+public class Convert
+{
+    public double USD = 2.67;
+    public double EUR = 2.87;
+
+}
